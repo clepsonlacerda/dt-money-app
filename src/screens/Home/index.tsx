bff86b525dc35f000ab9a96a2f7cd18162c1b357
Clepson <clepsonsantos9@gmail.com>
2025-12-26 22:03:00 -0300

    feat: implement DeleteModal and integrate transaction deletion functionality

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -5,10 +5,12 @@ import { useEffect } from "react";
 import { FlatList } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { ListHeader } from "./ListHeader";
+import { TransactionCard } from "./TransactionCard";
 
 export const Home = () => {
   const { handleLogout } = useAuthContext();
-  const { fetchCategories, fetchTransactions } = useTransactionContext();
+  const { fetchCategories, fetchTransactions, transactions } =
+    useTransactionContext();
   const { handlerError } = useErrorHandler();
 
   const handleFetchCategories = async () => {
@@ -29,8 +31,9 @@ export const Home = () => {
     <SafeAreaView className="flex-1 bg-background-primary">
       <FlatList
         className="bg-background-secondary"
-        data={[]}
-        renderItem={() => <></>}
+        data={transactions}
+        keyExtractor={({ id }) => `transaction-${id}`}
+        renderItem={({ item }) => <TransactionCard transaction={item} />}
         ListHeaderComponent={ListHeader}
       />
     </SafeAreaView>
